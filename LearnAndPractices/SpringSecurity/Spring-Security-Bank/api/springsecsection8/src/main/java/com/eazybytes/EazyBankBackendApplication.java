a7dd69efcc2c035cfe3934ec82d32d1d8e6d9422
MinhTranNTT <tvhuuminhntt@gmail.com>
2024-11-23 17:23:42 +0700

    invocation authorization

--- LearnAndPractices/SpringSecurity/Spring-Security-Bank/api/springsecsection8/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ LearnAndPractices/SpringSecurity/Spring-Security-Bank/api/springsecsection8/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -2,9 +2,11 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(jsr250Enabled = true,securedEnabled = true)
 // @EnableWebSecurity(debug = false)
 public class EazyBankBackendApplication {
 
