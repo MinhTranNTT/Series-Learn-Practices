f4bfe7db23fff1fb2f16586d2811477cc86782cf
MinhTranNTT <tvhuuminhntt@gmail.com>
2024-11-26 07:21:57 +0700

    start impl

--- hands-on-pet/daocao/pet_study/pet_auth/src/main/java/com/pet/auth/domain/service/impl/UmsMenuServiceImpl.java
+++ hands-on-pet/daocao/pet_study/pet_auth/src/main/java/com/pet/auth/domain/service/impl/UmsMenuServiceImpl.java
@@ -2,11 +2,8 @@ package com.pet.auth.domain.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.pet.auth.domain.entity.UmsMenu;
-import com.pet.auth.domain.entity.UmsRole;
 import com.pet.auth.domain.mapper.UmsMenuMapper;
-import com.pet.auth.domain.mapper.UmsRoleMapper;
 import com.pet.auth.domain.service.IUmsMenuService;
-import com.pet.auth.domain.service.IUmsRoleService;
 import org.springframework.stereotype.Service;
 
 @Service
--- hands-on-pet/daocao/pet_study/pet_starter/src/main/java/com/pet/PetApplication.java
+++ hands-on-pet/daocao/pet_study/pet_starter/src/main/java/com/pet/PetApplication.java
@@ -1,4 +1,4 @@
-package com.stt.starter;
+package com.pet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
