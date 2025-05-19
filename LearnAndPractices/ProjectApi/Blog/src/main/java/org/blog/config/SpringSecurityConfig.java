dd332ede261507b16c40b28c76c2513acb23d51b
MinhTranNTT <tvhuuminhntt@gmail.com>
2025-05-19 21:15:05 +0700

    implement InMemoryUserDetailsManager

--- LearnAndPractices/ProjectApi/Blog/src/main/java/org/blog/config/SpringSecurityConfig.java
+++ LearnAndPractices/ProjectApi/Blog/src/main/java/org/blog/config/SpringSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +21,13 @@ public class SpringSecurityConfig {
                 .formLogin(flc -> flc.disable())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userRead = User.withUsername("user").password("{noop}123456").authorities("read").build();
+        UserDetails userAdmin = User.withUsername("admin").password("{noop}123456").authorities("admin").build();
+        return new InMemoryUserDetailsManager(userRead, userAdmin);
     }
 
 }
