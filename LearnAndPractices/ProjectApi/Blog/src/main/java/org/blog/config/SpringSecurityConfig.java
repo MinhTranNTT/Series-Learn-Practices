f648c96ace40fd8aa5efcbfc32e16b1db8a348ce
MinhTranNTT <tvhuuminhntt@gmail.com>
2025-05-19 22:50:41 +0700

    implement PasswordEncoder

--- LearnAndPractices/ProjectApi/Blog/src/main/java/org/blog/config/SpringSecurityConfig.java
+++ LearnAndPractices/ProjectApi/Blog/src/main/java/org/blog/config/SpringSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,8 +28,13 @@ public class SpringSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails userRead = User.withUsername("user").password("{noop}123456").authorities("read").build();
-        UserDetails userAdmin = User.withUsername("admin").password("{noop}123456").authorities("admin").build();
+        UserDetails userAdmin = User.withUsername("admin").password("{bcrypt}$2a$12$SBQ6B4WuzbNaMYdU0sGvYuy1z/iCoq0Ll29.meIBndugoYezBwoL6").authorities("admin").build();
         return new InMemoryUserDetailsManager(userRead, userAdmin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
